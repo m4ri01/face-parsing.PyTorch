b0ff181f9845104cae54d1af928b0b3ad230248c
Melki Mario Gulo <elhackerz.9je.19@gmail.com>
2023-08-25 15:00:58 +0700

    add eyeshadow mask

--- test.py
+++ test.py
@@ -13,6 +13,37 @@ from PIL import Image
 import torchvision.transforms as transforms
 import cv2
 
+def change_color_gray(image,parsing,parts):
+    b1,g1,r1 = [255,255,255]
+    tar_color1 = np.zeros_like(image)
+    tar_color1[:,:,0] = b1
+    tar_color1[:,:,1] = g1
+    tar_color1[:,:,2] = r1
+    
+    tar_color2 = np.zeros_like(image)
+    parts = np.array(parts)
+    tar_color2[np.isin(parsing,parts)] = tar_color1[np.isin(parsing,parts)]
+    return tar_color2
+
+def dilate_image(image,kernel_size=(5,5),iterate=1):
+    dilation = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones(kernel_size,np.uint8)
+    dilation = cv2.dilate(dilation,kernel,iterations=iterate)
+    return dilation
+
+def change_eyeshadow(dilation_image,parsing,parts):
+    # grayscale_image = cv2.cvtColor(grayscale_image,cv2.COLOR_BGR2GRAY)
+    tar_color = np.zeros_like(dilation_image)
+    
+    dilation_image[np.isin(parsing,parts)] = tar_color[np.isin(parsing,parts)]
+    return dilation_image
+
+def add_parsing_eyeshadow(image,parsing):
+    index = np.where(image == 255)
+    parsing[index[0],index[1]] = 19
+    return parsing
+
+
 def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_results/parsing_map_on_im.jpg'):
     # Colors for all 20 parts
     part_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
@@ -30,11 +61,17 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
     vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
     vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
     vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 255
+    grayscale_image = change_color_gray(vis_im,vis_parsing_anno,[4,5])
+    dilation_image = dilate_image(grayscale_image,kernel_size=(5,5),iterate=5)
+    eyeshadow_image = change_eyeshadow(dilation_image,vis_parsing_anno,[4,5])
+    vis_parsing_add_eyeshadow = add_parsing_eyeshadow(eyeshadow_image,vis_parsing_anno)
 
-    num_of_class = np.max(vis_parsing_anno)
+
+
+    num_of_class = np.max(vis_parsing_add_eyeshadow)
 
     for pi in range(1, num_of_class + 1):
-        index = np.where(vis_parsing_anno == pi)
+        index = np.where(vis_parsing_add_eyeshadow == pi)
         vis_parsing_anno_color[index[0], index[1], :] = part_colors[pi]
 
     vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
@@ -43,7 +80,7 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
 
     # Save result or not
     if save_im:
-        cv2.imwrite(save_path[:-4] +'.png', vis_parsing_anno)
+        cv2.imwrite(save_path[:-4] +'.png', vis_parsing_add_eyeshadow)
         cv2.imwrite(save_path, vis_im, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
     # return vis_im
@@ -56,7 +93,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
     net.cuda()
-    save_pth = osp.join('res/cp', cp)
+    save_pth = osp.join('./cp', cp)
     net.load_state_dict(torch.load(save_pth))
     net.eval()
 
@@ -79,12 +116,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
             vis_parsing_maps(image, parsing, stride=1, save_im=True, save_path=osp.join(respth, image_path))
 
 
-
-
-
-
-
 if __name__ == "__main__":
-    evaluate(dspth='/home/zll/data/CelebAMask-HQ/test-img', cp='79999_iter.pth')
+    evaluate(dspth='./src', cp='79999_iter.pth')
 
 
